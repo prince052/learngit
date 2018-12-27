c7b0e186d820f6677d7981645c4c7fdb156f3673
Ryan <ryanxu2012@126.com>
2018-12-27 13:38:55 +0800

    wrote a readme file

---
+++ common/UTCtest/src/com/vanke/test/Utc.java
@@ -0,0 +1,19 @@
+package com.vanke.test;
+
+
+
+import org.junit.Test;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Utc {
+
+    @Test
+    public void t1(){
+        Date date = new Date();
+
+        }
+
+    }
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+git is a free version control system
